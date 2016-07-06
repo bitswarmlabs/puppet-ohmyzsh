15915f52539c98dddd5e1222f8e69d50aaaddd70
Reuben Avery <ravery@bitswarm.io>
2016-07-06 11:27:44 -0400

    debugging

--- manifests/uninstall.pp
+++ manifests/uninstall.pp
@@ -18,11 +18,13 @@ define ohmyzsh::uninstall(
 
   exec { "cp -f ${home}/.zshrc.orig ${home}/.zshrc && rm -f ${home}/.zshrc.orig":
     path   => ['/bin', '/usr/bin'],
+    user    => $name,
     onlyif => "test -d ${home}/.oh-my-zsh && test -e ${home}/.zshrc-orig"
   }
   ~>
   exec { "cp -f /etc/zsh/newuser.zshrc.recommended ${home}/.zshrc && rm -rf ${home}/.oh-my-zsh":
     path   => ['/bin', '/usr/bin'],
+    user    => $name,
     onlyif => "test -d ${home}/.oh-my-zsh && test ! -e ${home}/.zshrc.orig"
   }
   ~>
